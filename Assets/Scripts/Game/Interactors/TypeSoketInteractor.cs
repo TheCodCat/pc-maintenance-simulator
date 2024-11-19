baf7d143360aa2d52efde8a10ddc3466c3382961
TheCodCat <166803529+TheCodCat@users.noreply.github.com>
2024-11-19 17:59:45 +0400

    сделана правильная последовательность

--- Assets/Scripts/Game/Interactors/TypeSoketInteractor.cs
+++ Assets/Scripts/Game/Interactors/TypeSoketInteractor.cs
@@ -9,23 +9,49 @@ public class TypeSoketInteractor : XRSocketInteractor
     [SerializeField] private LevelController _levelController;
     [Header("Òåã ïğèñîåäèíåíèÿ")]
     [SerializeField] private TypePCSocket _typePCSocket;
+    [Header("Çàâèñÿùèå êîìïîíåíòû")]
+    [SerializeField] private TypeSoketInteractor _lastSelectableÑomponent;
+    [SerializeField, Tooltip("Íóæíî ëè óñòàíîâèòü äğóãîé êîìïîíåíò äëÿ óñòàíîâêè")] private bool _isSelectable;
+    [SerializeField] private bool _isSetup;
     [Header("Óñëîâèÿ çàìåíû")]
     [SerializeField] private PCXRGrapInteractable _requiredÑomponent;
     [SerializeField, Tooltip("Íóæíà ëè çàìåíà äà/íåò")] protected bool _required;
+    public bool IsSetup
+    {
+        get
+        {
+            return _isSetup;
+        }
+        set
+        {
+            _isSetup = value;
+        }
+    }
+
 
     private PCXRGrapInteractable _currentÑomponent;
 
     public override bool CanHover(IXRHoverInteractable interactable)
     {
         PCXRGrapInteractable component = interactable.transform.GetComponent<PCXRGrapInteractable>();
-        return base.CanHover(interactable) && component.GetSocket().Equals(_typePCSocket);
+        bool isSetup = _lastSelectableÑomponent != null ? _lastSelectableÑomponent.IsSetup : true;
+        return base.CanHover(interactable) && component.GetSocket().Equals(_typePCSocket) && isSetup;
     }
 
     public override bool CanSelect(IXRSelectInteractable interactable)
     {
         _currentÑomponent = interactable.transform.GetComponent<PCXRGrapInteractable>();
+        bool isSetup = _lastSelectableÑomponent != null ? _lastSelectableÑomponent.IsSetup : true;
+        return base.CanSelect(interactable) && _currentÑomponent.GetSocket().Equals(_typePCSocket) && isSetup;
+    }
 
-        return base.CanSelect(interactable) && _currentÑomponent.GetSocket().Equals(_typePCSocket);
+    public void onSelected(SelectEnterEventArgs selectEnterEventArgs)
+    {
+        IsSetup = true;
+    }
+    public void OnDeselected(SelectExitEventArgs selectExitEventArgs)
+    {
+        IsSetup = false;
     }
     public void FixSelectable(SelectEnterEventArgs selectEnterEventArgs)
     {
