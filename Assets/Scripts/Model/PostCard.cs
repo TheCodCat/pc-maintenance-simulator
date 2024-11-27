da44d722f385d1eb0dd65628d48a1f67e444bbe5
TheCodCat <166803529+TheCodCat@users.noreply.github.com>
2024-11-27 11:42:01 +0400

    апап

--- Assets/Scripts/Model/PostCard.cs
+++ Assets/Scripts/Model/PostCard.cs
@@ -1,24 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public abstract class PostCard : MonoBehaviour, IPost, IActivated
+public class PostCard : MonoBehaviour, IPost, IActivated
 {
     [SerializeField] private AudioClip _audioClip;
     [SerializeField] private AudioSource _audioSource;
+    [Header("Текстовая ошибка")]
+    [SerializeField] private Text _text;
+    [SerializeField] private string _title;
 
     public void Activate()
     {
         ActivePost(_audioClip);
+        _text.text = _title;
     }
     public void Deactivate()
     {
+        _text.text = string.Empty;
     }
 
     public void ActivePost(AudioClip post)
     {
         _audioSource.clip = post;
-        _audioSource.Play();
+        _audioSource?.Play();
     }
 
 }
